ed7fa8e80e1c0625fbf07a75821acd5672ff4735
Your Name <you@example.com>
2022-03-25 08:33:53 -0700

    add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+import { BsFillCameraFill } from 'react-icons/bs';
+
+const Header = () => {
+  return (
+    <div className='nav bg-light'>
+    <h3 className='fw-bold'>Camera Store < BsFillCameraFill className='icon'/></h3>
+    
+    </div>
+  )
+}
+
+export default Header
