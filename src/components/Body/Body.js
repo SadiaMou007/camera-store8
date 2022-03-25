90e016191a0e3379f17e5a8dd7557286e084f83c
Your Name <you@example.com>
2022-03-25 10:06:05 -0700

    show card info

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -2,22 +2,24 @@ import React, { useEffect, useState } from "react";
 import Camera from "./Camera/Camera";
 import Cart from "./Cart/Cart";
 const Body = () => {
-const [cameras,setCameras]=useState([])
-useEffect(()=>{
-    fetch('data.json')
-    .then(res=>res.json())
-    .then(data=>setCameras(data))
-},[])
+  const [cameras, setCameras] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setCameras(data));
+  }, []);
   return (
     <div className="container bg-light ">
       <div className="row">
-        <div className="col-lg-8 col-sm-8">
-{
-    cameras.map(camera=><Camera key={camera.id} camera={camera}></Camera>)
-}
+        <div className="col-lg-9 col-sm-9">
+          <div className="row my-5 g-2">
+            {cameras.map((camera) => (
+              <Camera key={camera.id} camera={camera}></Camera>
+            ))}
+          </div>
         </div>
-        <div className="col-lg-4 col-sm-4">
-        <Cart></Cart>
+        <div className="col-lg-3 col-sm-3">
+          <Cart></Cart>
         </div>
       </div>
     </div>
--- src/components/Body/Camera/Camera.js
+++ src/components/Body/Camera/Camera.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from "react";
+import "./Camera.css";
+import { BsCartCheckFill } from "react-icons/bs";
 
-const Camera = (props) => {
-    console.log(props.camera);
+const Camera = ({ camera }) => {
+  const { name, img, price } = camera;
   return (
-    <div></div>
-  )
-}
+    <div className="col-lg-4 col-md-6 col-sm-12 col-12 my-4 p-2">
+      <div className="">
+        <div class="card">
+          <img className="camera-img" src={img} alt="" />
+          <div class="card-body">
+            <h5 class="card-title name my-3">{name}</h5>
+            <p class="card-text fw-bold">Price: ${price}</p>
+          </div>
+          <div >
+            <button className="bg-dark p-2 w-100 border-0 text-white fw-bold bg-opacity-50">
+              Add To Cart <BsCartCheckFill className="cart-icon"/>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Camera
+export default Camera;
